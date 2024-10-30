c3687be03eb341263db32e31a9c4b0fd033637f9
Seventh-Sense <1577257077@qq.com>
2024-10-30 11:16:38 +0800

    modified

--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,14 @@
-import { defineConfig } from 'vite'
-import vue from '@vitejs/plugin-vue'
+import { defineConfig } from "vite";
+import vue from "@vitejs/plugin-vue";
+import path from "path";
+import { createSvg } from "./src/icons/svgIcon";
 
 // https://vite.dev/config/
 export default defineConfig({
-  plugins: [vue()],
-})
+  plugins: [vue(), createSvg("./src/icons/svg/")],
+  resolve: {
+    alias: {
+      "@": path.resolve(__dirname, "./src"),
+    },
+  },
+});
